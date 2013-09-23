c6017a4d9446c0d0058e54dc4af1b760f2e2005d
OhMedrano <Omedrano564@gmail.com>
2013-09-22 22:53:16 -0400

    adding new code

--- scales2.rb
+++ scales2.rb
@@ -3,16 +3,14 @@ text = gets.chomp
 
 scale = text.split ("")
 dorian = scale.rotate
-phyrgian = scale.rotate!(2)
-
-
+phyrgian = scale.rotate(2)
 
 puts "Would you like to know the dorian mode of it? press 1, press 2 for the phyrigan"
 answer = gets.chomp
 
 
 if answer == "1"
-	print dorian
+	print "Your mode is Dorian, #{dorian} and your notes for the minor chord is #{dorian[0]}, #{dorian[2]}, and #{dorian[4]}"
 elsif answer == "2"
 	print "Your mode is Phyrgian, #{phyrgian} and your notes for the chord is #{phyrgian[0]},#{phyrgian[2]}, and #{phyrgian[4]}"
 else 
