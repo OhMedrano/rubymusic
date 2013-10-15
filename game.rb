8353a453c96e4a8fd886c0f5d8776d8fa1bdda98
OhMedrano <Omedrano564@gmail.com>
2013-10-15 19:30:13 -0400

    added more story

--- game.rb
+++ game.rb
@@ -1,6 +1,9 @@
-require './weapons.rb'
+require './weapclass.rb'
 require './charclass.rb'
 
+sword1 = Weapon.new({type:"Sword", dmg:38, critic:20, name:"Sword of flames"})
+
+shield1 = Armor.new({type:"Sheild", defen:40})
 
 puts "Welcome to the world of this game, whose title is still in construction!"
 
@@ -22,3 +25,24 @@ char1 = Character.new({name: name, sex: gen, age: age})
 puts char1.name 
 puts char1.age 
 puts char1.sex
+
+
+puts "Is this information correct?"
+ans = gets.chomp
+
+if ans == "no"
+	puts "Restart the game"
+else
+	puts "awesome!"
+end
+
+
+story_heredoc = <<OMG
+
+A long time ago, there was a sacred weapon...
+
+			the #{sword1.name}!
+
+OMG
+
+puts story_heredoc
---
+++ gemfile.rb
@@ -0,0 +1,6 @@
+source 'rubygems.org'
+ruby '2.0.0'
+
+gem 'thor'
+gem 'cocaine'
+gem 'chronic'
